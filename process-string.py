84cd6189b83f2f4ce2c9f838f19ecf5003266134
altradCS <145883008+altradCS@users.noreply.github.com>
2024-01-24 11:37:38 +0700

    Update and rename README.md to process-string.py

---
+++ process-string.py
@@ -0,0 +1,26 @@
+# Application 1
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+
+
+# Application 2
+#  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
+
+# Examples
+# "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+# "h-o" ➞ "hijklmno"
+# "Q-Z" ➞ "QRSTUVWXYZ"
+# "J-J" ➞ "J"
+# Notes A hyphen will separate the two letters in the string.
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+user_range = input("Enter a range of letters (e.g., a-z): ")
+...
+...
+
