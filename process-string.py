a5442c7de09442e1cda72c2ecec3f4ad40ced3b6
Nikola-Limpet <156181712+Nikola-Limpet@users.noreply.github.com>
2024-01-27 21:15:07 +0700

    Update process-string.py
    done

--- process-string.py
+++ process-string.py
@@ -16,15 +16,16 @@ print(double_char)
 # # Application 2
 # #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
 
-# # Examples
-# # "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
-# # "h-o" ➞ "hijklmno"
-# # "Q-Z" ➞ "QRSTUVWXYZ"
-# # "J-J" ➞ "J"
-# # Notes A hyphen will separate the two letters in the string.
+# Examples
+# "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+# "h-o" ➞ "hijklmno"
+# "Q-Z" ➞ "QRSTUVWXYZ"
+# "J-J" ➞ "J"
+# Notes A hyphen will separate the two letters in the string.
 
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-start, end = alphabet.split('-')
 user_range = input("Enter a range of letters (e.g., a-z): ")
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+start , end = user_range.split("-")
+result_string = alphabet[alphabet.index(start) : alphabet.index(end) + 1]
+print(result_string)
 
