769b16971fd2786bf4e0f5f50f7d61d01016841f
altradCS <145883008+altradCS@users.noreply.github.com>
2024-03-28 13:46:34 +0700

    Create listapp.py

---
+++ listapp.py
@@ -0,0 +1,36 @@
+import random
+
+######## Custom Shuffle #############
+def custom_shuffle(lst):
+    # Add your algorithm................
+ 
+    return shuffled_lst
+
+
+
+####### Remove Duplicated Values ############
+def remove_duplicates(lst):
+    # add your algorimth...............
+
+    return unique_lst
+
+
+####### sort list in ascending ############
+def sort_list(lst):
+    # add your code......
+
+    return sorted_lst
+
+
+
+  
+# Example usage:
+original_list = [1, 2, 3, 4, 5, 5, 6, 7]
+shuffled_list = custom_shuffle(original_list)
+print("Shuffled list:", shuffled_list)
+
+unique_list = remove_duplicates(shuffled_list)
+print("List with duplicates removed:", unique_list)
+
+sorted_list = sort_list(shuffled_list)
+print("Sorted list in ascending order:", sorted_list)
